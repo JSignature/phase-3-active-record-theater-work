12a37eb6e6ca7911056857ce8e1d4f93649431da
Jimmy Andreas <jimmysignature@gmail.com>
2022-08-14 16:50:52 -0400

    Completed Assignment

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,20 +1,38 @@
 class Role < ActiveRecord::Base
     has_many :auditions
 
+    # Circle back to this, some ideas on how to get started
 
-def actors
-    actorArr=[]
-  
-    const actorList = self.audtions.map do |actor|
-        audition.actor
+    # for actors and locations do loop and shovel into an array.
+    # for role lead loop through and find hired = true. Look at the "any" method . return hire or return string
+    # understudy maybe push everyone into an array and then do [index]
+
+    def auditions
+
+        self.Auditions
+    end
+
+    def actors
+
+        self.Auditions.actors
 
     end
-    actorArr << actorList
+
+    def locations
+
+        self.Auditions.locations
+    end
+
+    def lead
+
+    end
+
+    def understudy
+
+    end
+
+
+
 end
-end
 
 
-
-# for actors and locations do loop and shovel into an array.
-# for role lead loop through and find hired = true. Look at the "any" method . return hire or return string
-# understudy maybe push everyone into an array and then do [index]
